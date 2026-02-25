b23ee391cc5b0a0eb1406eaa4f61eabd64241284
Elmane091203 <djaanffarhoumadielmane@gmail.com>
2026-02-25 05:03:35 +0000

    initial project

---
+++ WebApi/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿namespace WebApi.Controllers;
+
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models.Users;
+using WebApi.Services;
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private IUserService _userService;
+    private IMapper _mapper;
+    public UsersController(IUserService userService, IMapper mapper)
+    {
+        _userService = userService;
+        _mapper = mapper;
+    }
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var users = _userService.GetAll();
+        return Ok(users);
+    }
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var user = _userService.GetById(id);
+        return Ok(user);
+    }
+    [HttpPost]
+    public IActionResult Create(CreateRequest model)
+    {
+        _userService.Create(model);
+        return Ok(new { message = "User created" });
+    }
+    [HttpPut]
+    public IActionResult Update(UpdateRequest model)
+    {
+        _userService.Update(model);
+        return Ok(new { message = "User updated" });
+    }
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        _userService.Delete(id);
+        return Ok(new { message = "User deleted" });
+    }
+}
---
+++ WebApi/Helpers/DataContext.cs
@@ -0,0 +1,39 @@
+﻿namespace WebApi.Helpers;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Entities;
+
+public class DataContext : DbContext
+{
+    //protected readonly IConfiguration Configuration;
+    //public DataContext(IConfiguration configuration)
+    //{
+    //    Configuration = configuration;
+    //}
+    //protected override void OnConfiguring(DbContextOptionsBuilder options)
+    //{
+    //    // in memory database used for simplicity, change to a real db production applications
+    //    //options.UseInMemoryDatabase("TestDb");
+    //    options.UseNpgsql(Configuration.GetConnectionString("DbCahierTexteContext"));
+    //}
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configuration de l'entité User
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("users");
+
+            entity.HasIndex(e => e.Email).IsUnique();
+
+            entity.Property(e => e.Role)
+                .HasConversion<string>();
+        });
+    }
+}
